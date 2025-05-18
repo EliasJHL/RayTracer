9f285002fdce3de880ee550a124a176af9b3d718
Enzo <enzo.lobato-coutinho@epitech.eu>
2025-05-18 16:42:14 +0200

    [ADD]adding RenderingContext.hpp

---
+++ include/RenderingContext.hpp
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2025
+** B-OOP-400-MPL-4-1-raytracer-elias-josue.hajjar-llauquen
+** File description:
+** RenderingContext
+*/
+
+#ifndef RENDERING_CONTEXT_HPP
+#define RENDERING_CONTEXT_HPP
+
+#include <memory>
+#include "RenderingState.hpp"
+
+class RenderingContext {
+private:
+    std::unique_ptr<IRenderingState> currentState;
+
+public:
+    RenderingContext(std::unique_ptr<IRenderingState> initialState)
+        : currentState(std::move(initialState)) {}
+
+    void setState(std::unique_ptr<IRenderingState> newState) {
+        currentState = std::move(newState);
+    }
+
+    void execute() {
+        if (currentState) {
+            currentState->handle(*this);
+        }
+    }
+};
+
+#endif // RENDERING_CONTEXT_HPP
